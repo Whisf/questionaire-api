145765b7d505e4c2046587a1cd5017124fa9b62a
Whisf <95611477+Whisf@users.noreply.github.com>
2022-03-31 17:06:58 +0700

    Connect TypeOrm and Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './Modules/Auth/auth.module';
 import { QuestionModule } from './Modules/question/question.module';
 import { UserModule } from './Modules/User/user.module';
 
 @Module({
-  imports: [UserModule, AuthModule, QuestionModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '156327',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+    QuestionModule,
+  ],
 })
 export class AppModule {}
