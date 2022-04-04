2569b26be92b5ae603b1a71a7d7b2e4e91e969b8
deko <tam.nguyen.cdt@gmail.com>
2022-04-04 11:05:55 +0700

    feat-4

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
-import { JwtStrategy } from './jwt.strategy'
-import { JwtModule } from '@nestjs/jwt'
-import { PassportModule } from '@nestjs/passport'
 
 @Module({
-  imports: [
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.AUTHZ_CLIENT_SECRET,
-    }),
-  ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService],
   exports: [],
 })
 export class AuthModule {}
