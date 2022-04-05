70db5c776a0ad6b3198fc34ca3bc662cd031f6c0
Whisf <95611477+Whisf@users.noreply.github.com>
2022-04-05 16:26:07 +0700

    fix update-question

--- src/question/dto/update-question.dto.ts
+++ src/question/dto/update-question.dto.ts
@@ -1,15 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types'
-import { IsArray, IsString } from 'class-validator'
+import { IsArray, IsOptional, IsString } from 'class-validator'
 import { Answer } from 'src/entities/answer.entity'
 import { CreateQuestionDto } from './create-question.dto'
 
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
-  // @IsString()
+export class UpdateQuestionDto {
+  @IsString()
+  @IsOptional()
   description?: string
 
   @IsString()
+  @IsOptional()
   category?: string
 
   @IsArray()
+  @IsOptional()
   answers?: Answer[]
 }
