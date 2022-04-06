8c4b62bc2e560bc2d5fbc12be4aea0b7018fa240
deko <tam.nguyen.cdt@gmail.com>
2022-04-06 12:58:18 +0700

    feat-20.3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getUserState(@Req() req: Request): Promise<User | undefined> {
+  getUserState(@Req() req: Request): Promise<User | string> {
     return this.appService.getUserState(req)
   }
 
